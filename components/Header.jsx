d5f8c68aa9c77d026eb0a18cbd6ff575a007d564
Suhejb <fejziusuhejb12@gmail.com>
2023-05-02 23:19:42 +0200

    vans-life

---
+++ components/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react"
+import { Link, NavLink } from "react-router-dom"
+import AvatarIcon from "/assets/images/avatar-icon.png"
+import { BsPower } from "react-icons/bs"
+
+export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+    
+    return (
+        <header>
+            <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
+                <NavLink 
+                    to="host"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink 
+                    to="about"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    About
+                </NavLink>
+                <NavLink 
+                    to="vans"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+                <Link to="login" className="login-link">
+                    <img 
+                        src={AvatarIcon}
+                        className="login-icon"
+                    />
+                </Link>
+                <button className="logout" onClick={fakeLogOut}><BsPower /></button>
+            </nav>
+        </header>
+    )
+}
